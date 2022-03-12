77b4be6bde158514386dc91d27e6d6b938b67fac
induna-crewneck <66157637+induna-crewneck@users.noreply.github.com>
2022-03-12 12:08:37 +0100

    Update UpChecker.py

--- UpChecker.py
+++ UpChecker.py
@@ -1,6 +1,6 @@
 # python
 
-# imports ----------------------------------------------------------
+# imports ----------------------------------------------------------------------------------------
 import subprocess
 import requests
 import os
@@ -9,27 +9,45 @@ import json
 from urllib2 import urlopen
 import datetime
 
-# define Variables -------------------------------------------------
+# define Variables -------------------------------------------------------------------------------
 TELEGRAM_BOT = 'telegram_bot_token'
 TELEGRAM_ME  = 'target_telegram_user_id'
-TELEGRAM_MSG = 'messagecontent'
+TELEGRAM_MSG = 'empty message'
 OFFLINEIDENT = '100% packet loss'
 TIMESTAMP = datetime.datetime.now()
 
-# define Error handling function -----------------------------------
+# define Error handling function -----------------------------------------------------------------
 def offline():
     # Attemting to send message (can't hurt to try, right?)
-    TELEGRAM_MSG='ping timed out \n' + str(TIMESTAMP)
+    TELEGRAM_MSG = 'ping timed out \n' + str(TIMESTAMP)
     url = 'https://api.telegram.org/bot' + str(TELEGRAM_BOT) + '/sendMessage?chat_id=' + str(TELEGRAM_ME) + '&text=' + str(TELEGRAM_MSG)
-    x = requests.post(url)
-    # Logging to /root/OnlineStatusLog.txt
-    file_object = open('OnlineStatusLog.txt', 'a')
-    file_object.write(' \n-------------------------------------------------------------------------------------------------------- \n' + str(TIMESTAMP) + '    PING ERROR    \n' + str(ping))
+    y = requests.post(url)
+    # 'y' is now the reponse of sending the telegram message
+    # show detailed reponse:    print y.text
+    # show response code:       print y
+    
+    # Logging to OnlineStatusLog.txt
+    file_object = open('UpCheckerLog.txt', 'a')
+    file_object.write(' \n-------------------------------------------------------------------------------------------------------- \n' + str(TIMESTAMP) + '    PING ERROR    \n' + str(error) + ' \n' + str(ping))
     file_object.close()
     # Rebooting system
     os.system('reboot')
 
-# ping host url ----------------------------------------------------
+def online():
+    # Sending update message
+    TELEGRAM_MSG='Pi is online (routine check) \n'+ str(TIMESTAMP) + ' \nIP : {3} \nRegion : {0} \nCountry : {1} \nCity : {2}'.format(region,country,city,IP)
+    url = 'https://api.telegram.org/bot' + str(TELEGRAM_BOT) + '/sendMessage?chat_id=' + str(TELEGRAM_ME) + '&text=' + str(TELEGRAM_MSG)
+    y = requests.post(url)
+    # 'y' is now the reponse of sending the telegram message
+    # show detailed reponse:    print y.text
+    # show response code:       print y
+    
+    # Logging to OnlineStatusLog.txt
+    file_object = open('UpCheckerLog.txt', 'a')
+    file_object.write(' \n-------------------------------------------------------------------------------------------------------- \n' + str(TIMESTAMP) + '    PING SUCCESSFUL    \nIP : {3} \nRegion : {0} \nCountry : {1} \nCity : {2}'.format(region,country,city,IP))
+    file_object.close()
+
+# ping host url ----------------------------------------------------------------------------------
 host = 'www.google.com'
 ping = subprocess.Popen(
     ['ping', '-c', '4', host],
@@ -39,7 +57,7 @@ ping = subprocess.Popen(
 ping, error = ping.communicate()
 # 'ping' is the result of the ping command now
 
-# Get IP address ---------------------------------------------------
+# Get IP address ---------------------------------------------------------------------------------
 url = 'http://ipinfo.io/json'
 response = urlopen(url)
 data = json.load(response)
@@ -48,13 +66,9 @@ city = data['city']
 country=data['country']
 region=data['region']
 
-# search ping result for offline code ------------------------------
+# search ping result for offline code ------------------------------------------------------------
 if OFFLINEIDENT in ping: X = offline()
-else: TELEGRAM_MSG='Pi is online (routine check) \n'+ str(TIMESTAMP) + ' \nIP : {3} \nRegion : {0} \nCountry : {1} \nCity : {2}'.format(region,country,city,IP)
+else: X = online()
 
-# send results to telegram -----------------------------------------
-url = 'https://api.telegram.org/bot' + str(TELEGRAM_BOT) + '/sendMessage?chat_id=' + str(TELEGRAM_ME) + '&text=' + str(TELEGRAM_MSG)
-x = requests.post(url)
-# 'x' is now the reponse of sending the telegram message
-# show detailed reponse:    print x.text
-# show response code:       print x
+
+
