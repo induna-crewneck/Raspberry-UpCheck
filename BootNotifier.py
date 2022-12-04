1168325ecbe2a46744aa60273f594fa43b3dd863
induna-crewneck <66157637+induna-crewneck@users.noreply.github.com>
2022-12-04 16:05:23 +0100

    Version 2.0
    Removed IP check, ordering excecution of UpChecker.py.
    This way changes to UpChecker.py don't need to be made here.

--- BootNotifier.py
+++ BootNotifier.py
@@ -1,4 +1,4 @@
-# Raspberry-BootNotifier v1.0
+# Raspberry-BootNotifier v2.0
 # Intended as part of Raspberry-UpCheck
 # github.com/induna-crewneck/Raspberry-UpCheck/
 # python
@@ -15,17 +15,8 @@ TELEGRAM_ME  = 'target_telegram_user_id'
 TELEGRAM_MSG = 'empty message'
 TIMESTAMP = datetime.datetime.now()
 
-# Get IP address and IP info
-url = 'http://ipinfo.io/json'
-response = urlopen(url)
-data = json.load(response)
-IP=data['ip']
-city = data['city']
-country=data['country']
-region=data['region']
-    
 # Sending update message via Telegram
-TELEGRAM_MSG='Pi just rebooted. \n'+ str(TIMESTAMP) + ' \nIP : {3} \n({2}, {0}, {1})'.format(region,country,city,IP)
+TELEGRAM_MSG='System rebooted. \n'+ str(TIMESTAMP)
 url = 'https://api.telegram.org/bot' + str(TELEGRAM_BOT) + '/sendMessage?chat_id=' + str(TELEGRAM_ME) + '&text=' + str(TELEGRAM_MSG)
 y = requests.post(url)
 # 'y' is now the reponse of sending the telegram message
@@ -34,5 +25,8 @@ y = requests.post(url)
     
 # Logging to OnlineStatusLog.txt
 file_object = open('Raspberry-UpCheck/UpCheckerLog.txt', 'a')
-file_object.write(' \n-------------------------------------------------------------------------------------------------------- \n' + str(TIMESTAMP) + '    SYSTEM REBOOTED    \nIP : {3} ({2}, {0}, {1})'.format(region,country,city,IP))
+file_object.write(' \n-------------------------------------------------------------------------------------------------------- \n' + str(TIMESTAMP) + '    SYSTEM REBOOTED')
 file_object.close()
+
+# Running UpChecker.py
+exec(open("/root/Raspberry-UpCheck/UpChecker.py").read())
